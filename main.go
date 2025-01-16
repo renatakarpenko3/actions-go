942bbe4fe8c6770f02a180a0f32aaff18e1b248a
Renata Karpenko <renatakarpenko2@gmail.com>
2025-01-16 15:00:08 -0500

    Files added

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World! This is a simple Go application.")
+	fmt.Fprintln(w, "Hello, World!")
 }
 
 func main() {
